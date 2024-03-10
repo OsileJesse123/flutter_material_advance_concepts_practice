2340abce163791e2bddbb46ef57851af7c4f8c58
Osile Jesse <jesseosile@gmail.com>
2024-03-10 19:46:15 +0100

    back drop screen ongoing

---
+++ lib/view/back_drop.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BackDropPage extends StatefulWidget {
+  const BackDropPage({super.key});
+
+  @override
+  State<BackDropPage> createState() => _BackDropPageState();
+}
+
+class _BackDropPageState extends State<BackDropPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
